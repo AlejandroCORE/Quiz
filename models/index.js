e2a43ed065d412a4ec1ee51ee78cc01a7ed27e29
COREalex <a.s.e.288@gmail.com>
2016-05-04 02:53:07 +0200

    Tema 10: Lista de preguntas

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ var Sequelize = require('sequelize');
 // Usar BBDD SQLite:
 //    DATABASE_URL = sqlite:///
 //    DATABASE_STORAGE = quiz.sqlite
+// Usar BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
 
 var url, storage;
 
@@ -27,25 +29,23 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync()
-    .then(function() {
+sequelize.sync().then(function() {
         // Ya se han creado las tablas necesarias.
-        return Quiz.count()
-                .then(function (c) {
+        return Quiz.count().then(function (c) {
                     if (c === 0) {   // la tabla se inicializa solo si está vacía
-                        return Quiz.create({ question: 'Capital de Italia',
-          	                                 answer: 'Roma'
-          	                               })
-                                   .then(function() {
+                        return Quiz.bulkCreate([ {question: 'Capital de Italia',   answer: 'Roma'},
+                                                 {question: 'Capital de Portugal', answer: 'Lisboa'},
+						 {question: 'Capital de España', answer: 'Madrid'},
+						 {question: 'Capital de Brasil', answer: 'Brasilia'}
+                                              ]).then(function() {
                                         console.log('Base de datos inicializada con datos');
                                     });
                     }
                 });
-    })
-    .catch(function(error) {
+    }).catch(function(error) {
         console.log("Error Sincronizando las tablas de la BBDD:", error);
         process.exit(1);
     });
 
 
-exports.Quiz = Quiz; // exportar definición de la tabla Quiz
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
